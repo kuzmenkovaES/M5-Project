94289f8b392c031f1b6d086d1e128df92145b756
Katerina Kuzmenkova <kuzmenkovaes@gmail.com>
2018-12-03 13:25:16 +0300

    hover menu

--- src/js/hover-menu.js
+++ src/js/hover-menu.js
@@ -12,19 +12,27 @@
         if (!ELEMENTS_SPAN[index]){
             ELEMENTS_SPAN[index] = element.querySelector("span");
             ELEMENTS_SPAN[index].style.height = `${heightNavLink}px`;
-            // ELEMENTS_SPAN[index].style.width = `${widthNavLink}px`;
+            ELEMENTS_SPAN[index].style.width = `${widthNavLink}px`;
         }
 
-        element.addEventListener("mouseover", event => {
-            // console.log(event)
+        element.addEventListener("mousemove", event => {
+            console.log(event)
             // console.log(element)
-            const left =  (event.pageX - element.offsetLeft);
+            console.log(index)
+            let left = event.offsetX - widthNavLink/2;
             ELEMENTS_SPAN[index].style.left = `${left}px`;
         });
 
-        element.addEventListener("mouseout", event => {
-            ELEMENTS_SPAN[index].style.left = event.pageX - element.offsetLeft + "px";
-        });
+        // element.addEventListener("mouseover", event => {
+        //     console.log(event)
+        //     console.log(element)
+        //     const left =  (event.pageX - element.offsetLeft);
+        //     ELEMENTS_SPAN[index].style.left = `${event.clientX}px`;
+        // });
+        //
+        // element.addEventListener("mouseout", event => {
+        //     ELEMENTS_SPAN[index].style.left = event.pageX - element.offsetLeft + "px";
+        // });
     });
 
 })();
